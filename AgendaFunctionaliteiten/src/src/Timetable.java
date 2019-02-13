29ee8213b2ebe1e8ce00a9a7f134ea25d0a95925
boris Korevaar <b.korevaar@avans.nl>
2019-02-13 12:36:09 +0100

    Add performance functie toegevoegd

--- AgendaFunctionaliteiten/src/src/Timetable.java
+++ AgendaFunctionaliteiten/src/src/Timetable.java
@@ -11,14 +11,34 @@ public class Timetable {
     public void addPerformance(Stage stage, Artist artist, int beginTime, int endTime){
 
         Performance temp = new Performance(stage, artist,beginTime,endTime);
-
-        if(performances.contains(stage)){
-
+        boolean doubleBooking = false;
+        boolean noOverlappingTime = true;
+        for(int i = 0; i < performances.size(); i++){
+            if(performances.get(i).getStage().equals(temp.getStage())){
+                if(performances.get(i).getBeginTime()<= temp.getBeginTime() && performances.get(i).getEndTime() <= temp.getBeginTime() || performances.get(i).getBeginTime()>= temp.getEndTime() && performances.get(i).getEndTime() >= temp.getEndTime()){
+                    noOverlappingTime = true;
+                }
+                else{
+                    noOverlappingTime = false;
+                }
+            }
+            if(performances.get(i).getArtist().equals(temp.getArtist())){
+                if(performances.get(i).getBeginTime() <= temp.getBeginTime()&&performances.get(i).getEndTime() <= temp.getBeginTime() || performances.get(i).getBeginTime() >= temp.getEndTime() && performances.get(i).getEndTime() >= temp.getEndTime()){
+                    doubleBooking = false;
+                }
+                else{
+                    doubleBooking = true;
+                }
+            }
         }
-
-        performances.add(new Performance(stage, artist, beginTime, endTime));
+        if(noOverlappingTime == true && doubleBooking == false) {
+            performances.add(new Performance(stage, artist, beginTime, endTime));
+        }
     }
 
+    public void editPerformance(Performance performance){
+
+    }
 
 }
 
