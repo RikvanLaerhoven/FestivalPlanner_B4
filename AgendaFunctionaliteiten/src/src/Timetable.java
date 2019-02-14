dc84b61a8f51bcdcb70ccc3846f108d7b4418c89
boris Korevaar <b.korevaar@avans.nl>
2019-02-14 09:49:30 +0100

    Meerdere editPerformance checks toegevoegd en add en remove functionaliteit

--- AgendaFunctionaliteiten/src/src/Timetable.java
+++ AgendaFunctionaliteiten/src/src/Timetable.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 public class Timetable {
 
-    public List<Performance> performances = new LinkedList<>();
+    private List<Performance> performances = new LinkedList<>();
 
+
+    public Timetable(){
+    }
 
     public void addPerformance(Stage stage, Artist artist, int beginTime, int endTime){
 
@@ -23,7 +26,7 @@ public class Timetable {
                 }
             }
             if(performances.get(i).getArtist().equals(temp.getArtist())){
-                if(performances.get(i).getBeginTime() <= temp.getBeginTime()&&performances.get(i).getEndTime() <= temp.getBeginTime() || performances.get(i).getBeginTime() >= temp.getEndTime() && performances.get(i).getEndTime() >= temp.getEndTime()){
+                if(performances.get(i).getBeginTime() <= temp.getBeginTime() && performances.get(i).getEndTime() <= temp.getBeginTime() || performances.get(i).getBeginTime() >= temp.getEndTime() && performances.get(i).getEndTime() >= temp.getEndTime()){
                     doubleBooking = false;
                 }
                 else{
@@ -36,10 +39,146 @@ public class Timetable {
         }
     }
 
-    public void editPerformance(Performance performance){
+    public void editPerformance(Performance performance, Artist artist, int beginTime, int endTime, Stage stage){
+        boolean doubleBooking = false;
+        boolean noOverlappingTime = true;
+        for(int i = 0 ; i < performances.size(); i++){
+            if(performances.get(i).getStage().equals(performance.getStage())){
+                if(performances.get(i).getBeginTime() <= performance.getBeginTime() && performances.get(i).getBeginTime() <= performance.getEndTime() || performances.get(i).getBeginTime() >= performance.getEndTime() && performances.get(i).getEndTime() >= performance.getEndTime()){
+                    noOverlappingTime = true;
+                }
+                else{
+                    noOverlappingTime = false;
+                }
+            }
+            if(performances.get(i).getArtist().equals(performance.getArtist())){
+                if(performances.get(i).getBeginTime() <= performance.getBeginTime() && performances.get(i).getBeginTime() <= performance.getEndTime() || performances.get(i).getBeginTime() >= performance.getEndTime() && performances.get(i).getEndTime() >= performance.getEndTime()){
+                    doubleBooking = false;
+                }
+                else{
+                    doubleBooking = true;
+                }
+            }
+            if(noOverlappingTime == true && doubleBooking == false){
+                performance.setBeginTime(beginTime);
+                performance.setEndTime(endTime);
+                performance.setStage(stage);
+                performance.setArtist(artist);
+            }
+        }
+    }
+    public void editPerformance(Performance performance, int beginTime, int endTime, Stage stage){
+        boolean doubleBooking = false;
+        boolean noOverlappingTime = true;
+        for(int i = 0 ; i < performances.size(); i++){
+            if(performances.get(i).getStage().equals(performance.getStage())){
+                if(performances.get(i).getBeginTime() <= performance.getBeginTime() && performances.get(i).getBeginTime() <= performance.getEndTime() || performances.get(i).getBeginTime() >= performance.getEndTime() && performances.get(i).getEndTime() >= performance.getEndTime()){
+                    noOverlappingTime = true;
+                }
+                else{
+                    noOverlappingTime = false;
+                }
+            }
+            if(performances.get(i).getArtist().equals(performance.getArtist())){
+                if(performances.get(i).getBeginTime() <= performance.getBeginTime() && performances.get(i).getBeginTime() <= performance.getEndTime() || performances.get(i).getBeginTime() >= performance.getEndTime() && performances.get(i).getEndTime() >= performance.getEndTime()){
+                    doubleBooking = false;
+                }
+                else{
+                    doubleBooking = true;
+                }
+            }
+            if(noOverlappingTime == true && doubleBooking == false){
+                performance.setBeginTime(beginTime);
+                performance.setEndTime(endTime);
+                performance.setStage(stage);
+            }
+        }
+    }
+    public void editPerformance(Performance performance, int beginTime, int endTime){
+        boolean doubleBooking = false;
+        boolean noOverlappingTime = true;
+        for(int i = 0 ; i < performances.size(); i++){
+            if(performances.get(i).getStage().equals(performance.getStage())){
+                if(performances.get(i).getBeginTime() <= performance.getBeginTime() && performances.get(i).getBeginTime() <= performance.getEndTime() || performances.get(i).getBeginTime() >= performance.getEndTime() && performances.get(i).getEndTime() >= performance.getEndTime()){
+                    noOverlappingTime = true;
+                }
+                else{
+                    noOverlappingTime = false;
+                }
+            }
+            if(performances.get(i).getArtist().equals(performance.getArtist())){
+                if(performances.get(i).getBeginTime() <= performance.getBeginTime() && performances.get(i).getBeginTime() <= performance.getEndTime() || performances.get(i).getBeginTime() >= performance.getEndTime() && performances.get(i).getEndTime() >= performance.getEndTime()){
+                    doubleBooking = false;
+                }
+                else{
+                    doubleBooking = true;
+                }
+            }
+            if(noOverlappingTime == true && doubleBooking == false){
+                performance.setBeginTime(beginTime);
+                performance.setEndTime(endTime);
+            }
+        }
 
     }
+    public void editPerformance(Performance performance, Stage stage){
+        boolean doubleBooking = false;
+        boolean noOverlappingTime = true;
+        for(int i = 0 ; i < performances.size(); i++){
+            if(performances.get(i).getStage().equals(performance.getStage())){
+                if(performances.get(i).getBeginTime() <= performance.getBeginTime() && performances.get(i).getBeginTime() <= performance.getEndTime() || performances.get(i).getBeginTime() >= performance.getEndTime() && performances.get(i).getEndTime() >= performance.getEndTime()){
+                    noOverlappingTime = true;
+                }
+                else{
+                    noOverlappingTime = false;
+                }
+            }
+            if(performances.get(i).getArtist().equals(performance.getArtist())){
+                if(performances.get(i).getBeginTime() <= performance.getBeginTime() && performances.get(i).getBeginTime() <= performance.getEndTime() || performances.get(i).getBeginTime() >= performance.getEndTime() && performances.get(i).getEndTime() >= performance.getEndTime()){
+                    doubleBooking = false;
+                }
+                else{
+                    doubleBooking = true;
+                }
+            }
+            if(noOverlappingTime == true && doubleBooking == false){
+                performance.setStage(stage);
+            }
+        }
+    }
+    public void editPerformance(Performance performance, Artist artist){
+        boolean doubleBooking = false;
+        boolean noOverlappingTime = true;
+        for(int i = 0 ; i < performances.size(); i++){
+            if(performances.get(i).getStage().equals(performance.getStage())){
+                if(performances.get(i).getBeginTime() <= performance.getBeginTime() && performances.get(i).getBeginTime() <= performance.getEndTime() || performances.get(i).getBeginTime() >= performance.getEndTime() && performances.get(i).getEndTime() >= performance.getEndTime()){
+                    noOverlappingTime = true;
+                }
+                else{
+                    noOverlappingTime = false;
+                }
+            }
+            if(performances.get(i).getArtist().equals(performance.getArtist())){
+                if(performances.get(i).getBeginTime() <= performance.getBeginTime() && performances.get(i).getBeginTime() <= performance.getEndTime() || performances.get(i).getBeginTime() >= performance.getEndTime() && performances.get(i).getEndTime() >= performance.getEndTime()){
+                    doubleBooking = false;
+                }
+                else{
+                    doubleBooking = true;
+                }
+            }
+            if(noOverlappingTime == true && doubleBooking == false){
+                performance.setArtist(artist);
+            }
+        }
+    }
 
+    public void removePerformance(Performance performance){
+        for(int i = 0; i< performances.size(); i++){
+            if(performances.get(i).equals(performance)){
+                performances.remove(i);
+            }
+        }
+    }
 }
 
 
