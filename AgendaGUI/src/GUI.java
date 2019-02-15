f515ae4e4cde1053b2186b0d13a3967b4c2ee2c7
Rik van Laerhoven <rikvanlaerhoven@hotmail.com>
2019-02-15 09:15:54 +0100

    tabInput afgemaakt, button actions toegevoegd, informatie additie methode voor tab data toegevoegd

--- AgendaGUI/src/GUI.java
+++ AgendaGUI/src/GUI.java
@@ -15,11 +15,15 @@ public class GUI extends Application {
     /** onderstaande variabelen horen bij de tab Input,
      * deze variabelen krijgen zodra op de enter knop wordt gedrukt de inhoud van het bijbehorden tekstveld.
      */
+    private GridPane gridPaneData = new GridPane();
     private String nameInput;
     private String genreInput;
     private String stageInput;
     private String popularityInput;
-    private String timeInput;
+    private String timeFromInput;
+    private String timeToInput;
+    private String fileLocation;
+    private int dataCounter = 1;
 
     public void start(Stage stage) throws Exception {
         try {
@@ -46,7 +50,7 @@ public class GUI extends Application {
             VBox popularity = new VBox();
             VBox time = new VBox();
             HBox tabData = new HBox();
-            GridPane gridPaneData = new GridPane();
+//            GridPane gridPaneData = new GridPane();
 
             name.getChildren().add(new Label(stringEditor("Name")));
             genre.getChildren().add(new Label(stringEditor("Genre")));
@@ -71,32 +75,44 @@ public class GUI extends Application {
             TextField nameTextfield = new TextField("Type name here");
             TextField genreTextfield = new TextField("Type genre here");
             TextField stageTextfield = new TextField("Type stage here");
+            TextField fileInputTextfield = new TextField("Type file path here");
             TextField popularityTextfield = new TextField("Type popularity here");
+            TextField timeFromTextfield = new TextField();
+            TextField timeToTextfield = new TextField();
 
             HBox content = new HBox();
             VBox values = new VBox();
             VBox textfields = new VBox();
+            VBox fileInput = new VBox();
+            VBox timeTextfields = new VBox();
+            HBox timeFromBox = new HBox();
+            HBox timeToBox = new HBox();
+
             values.getChildren().addAll(new Label("Name: "),new Label("Genre: "),new Label("Stage: "),new Label("Popularity: "),new Label("Time: "));
             values.setSpacing(29);
-            textfields.getChildren().addAll(nameTextfield,genreTextfield,stageTextfield,popularityTextfield);
+            timeFromBox.getChildren().addAll(new Label("From: "),timeFromTextfield);
+            timeToBox.getChildren().addAll(new Label("To:     "),timeToTextfield);
+            timeTextfields.getChildren().addAll(timeFromBox,timeToBox);
+            textfields.getChildren().addAll(nameTextfield,genreTextfield,stageTextfield,popularityTextfield,timeTextfields);
             textfields.setSpacing(20);
             content.getChildren().addAll(values,textfields);
             content.setSpacing(10);
 
             Button buttonEnter = new Button("Enter");
-            Button buttonImport = new Button("Import File");
+            Button buttonFileImport = new Button("Import File");
             buttonEnter.setScaleY(2);
             buttonEnter.setScaleX(2);
-            buttonImport.setScaleY(2);
-            buttonImport.setScaleX(2);
+            buttonFileImport.setScaleY(2);
+            buttonFileImport.setScaleX(2);
 
+            fileInput.getChildren().addAll(new Label("File: "),fileInputTextfield);
             GridPane gridPaneInput = new GridPane();
             gridPaneInput.setHgap(50);
             gridPaneInput.setVgap(50);
             gridPaneInput.add(content,1,1);
             gridPaneInput.add(buttonEnter,1,2);
-            gridPaneInput.add(buttonImport,3,2);
-
+            gridPaneInput.add(fileInput,2,1);
+            gridPaneInput.add(buttonFileImport,2,2);
             input.setContent(gridPaneInput);
 
 
@@ -119,8 +135,15 @@ public class GUI extends Application {
                 genreInput = genreTextfield.getText();
                 stageInput = stageTextfield.getText();
                 popularityInput = popularityTextfield.getText();
-                //iet voor de tijd
+                timeFromInput = timeFromTextfield.getText();
+                timeToInput = timeToTextfield.getText();
+                addData();
             });
+
+            buttonFileImport.setOnAction(event -> {
+                fileLocation = fileInputTextfield.getText();
+            });
+
         }
         catch (Exception e){
             System.out.println(e.getMessage());
@@ -133,5 +156,15 @@ public class GUI extends Application {
 
     public String stringEditor(String inputText){
         return "  " + inputText + "  ";
+    }
+
+    public void addData(){
+        dataCounter++;
+        gridPaneData.setGridLinesVisible(true);
+        gridPaneData.add(new Label(stringEditor(nameInput)),1,dataCounter);
+        gridPaneData.add(new Label(stringEditor(genreInput)),2,dataCounter);
+        gridPaneData.add(new Label(stringEditor(stageInput)),3,dataCounter);
+        gridPaneData.add(new Label(stringEditor(popularityInput)), 4, dataCounter);
+        gridPaneData.add(new Label(stringEditor(timeFromInput + " - "+ timeToInput)),5,dataCounter);
     }
 }
