11dc702f92a511f42ba7ff83cd05a1b677ace873
MartDierink <aam.dierink@student.avans.nl>
2019-02-23 13:44:35 +0100

    kleine fix voor info read

--- AgendaFunctionaliteiten/src/Performance/SaveFile.java
+++ AgendaFunctionaliteiten/src/Performance/SaveFile.java
@@ -5,46 +5,46 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class SaveFile  {
-        ArrayList<Artist> savelist1 = new ArrayList<>();
-    ArrayList<Artist> savelist2 = new ArrayList<>();
-    ArrayList<Artist> savelist3 = new ArrayList<>();
-    ArrayList<Artist> savelist4 = new ArrayList<>();
+        ArrayList<Performance> savelist1 = new ArrayList<>();
+    ArrayList<Performance> savelist2 = new ArrayList<>();
+    ArrayList<Performance> savelist3 = new ArrayList<>();
+    ArrayList<Performance> savelist4 = new ArrayList<>();
 
-    public ArrayList<Performance.Artist> getSavelist1() {
+    public ArrayList<Performance> getSavelist1() {
         return savelist1;
     }
 
-    public void setSavelist1(ArrayList<Performance.Artist> savelist1) {
+    public void setSavelist1(ArrayList<Performance> savelist1) {
         this.savelist1 = savelist1;
     }
 
-    public ArrayList<Performance.Artist> getSavelist2() {
+    public ArrayList<Performance> getSavelist2() {
         return savelist2;
     }
 
-    public void setSavelist2(ArrayList<Performance.Artist> savelist2) {
+    public void setSavelist2(ArrayList<Performance> savelist2) {
         this.savelist2 = savelist2;
     }
 
-    public ArrayList<Performance.Artist> getSavelist3() {
+    public ArrayList<Performance> getSavelist3() {
         return savelist3;
     }
 
-    public void setSavelist3(ArrayList<Performance.Artist> savelist3) {
+    public void setSavelist3(ArrayList<Performance> savelist3) {
         this.savelist3 = savelist3;
     }
 
-    public ArrayList<Performance.Artist> getSavelist4() {
+    public ArrayList<Performance> getSavelist4() {
         return savelist4;
     }
 
-    public void setSavelist4(ArrayList<Performance.Artist> savelist4) {
+    public void setSavelist4(ArrayList<Performance> savelist4) {
         this.savelist4 = savelist4;
     }
 
 
 
-    public void saveFile(Artist artist) {
+    public void saveFile(Performance performance) {
         try {
             File file1 = new File("scores.txt");
             File file2 = new File("scores.txt");
@@ -55,23 +55,23 @@ public class SaveFile  {
 
             if (GUI.saveFile == file1) {
 
-                output.print(artist);
-                this.savelist1.add(artist);
+                output.print(performance);
+                this.savelist1.add(performance);
             }
             if (GUI.saveFile == file2) {
 
-                output.print(artist);
-                this.savelist2.add(artist);
+                output.print(performance);
+                this.savelist2.add(performance);
             }
             if (GUI.saveFile == file3) {
 
-                output.print(artist);
-                this.savelist3.add(artist);
+                output.print(performance);
+                this.savelist3.add(performance);
             }
             if (GUI.saveFile == file4) {
 
-                output.print(artist);
-                this.savelist4.add(artist);
+                output.print(performance);
+                this.savelist4.add(performance);
             }
         }
         catch (Exception e){
