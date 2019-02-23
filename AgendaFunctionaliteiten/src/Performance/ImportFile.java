934831ae564a8a2234bd2374a488012f12da9d5c
MartDierink <aam.dierink@student.avans.nl>
2019-02-23 13:30:54 +0100

    start import maar eerst moet save button af

---
+++ AgendaFunctionaliteiten/src/Performance/ImportFile.java
@@ -0,0 +1,18 @@
+package Performance;
+
+public class ImportFile {
+    public void importFile(Artist artist){
+        if(GUI.importFile == file1) {
+            file1 = SaveFile.getSavelist1;
+        }
+        if(GUI.importFile == file2) {
+            file2 = SaveFile.getSavelist2;
+        }
+        if(GUI.importFile == file3) {
+            file3 = SaveFile.getSavelist3;
+        }
+        if(GUI.importFile == file4) {
+            file4 = SaveFile.getSavelist4;
+        }
+    }
+}
--- AgendaFunctionaliteiten/src/Performance/SaveFile.java
+++ AgendaFunctionaliteiten/src/Performance/SaveFile.java
@@ -4,9 +4,43 @@ import java.util.ArrayList;
 
 public class SaveFile  {
         ArrayList<Artist> savelist1 = new ArrayList<>();
-        ArrayList<Artist> savelist2 = new ArrayList<>();
-        ArrayList<Artist> savelist3 = new ArrayList<>();
-        ArrayList<Artist> savelist4 = new ArrayList<>();
+    ArrayList<Artist> savelist2 = new ArrayList<>();
+    ArrayList<Artist> savelist3 = new ArrayList<>();
+    ArrayList<Artist> savelist4 = new ArrayList<>();
+
+    public ArrayList<Performance.Artist> getSavelist1() {
+        return savelist1;
+    }
+
+    public void setSavelist1(ArrayList<Performance.Artist> savelist1) {
+        this.savelist1 = savelist1;
+    }
+
+    public ArrayList<Performance.Artist> getSavelist2() {
+        return savelist2;
+    }
+
+    public void setSavelist2(ArrayList<Performance.Artist> savelist2) {
+        this.savelist2 = savelist2;
+    }
+
+    public ArrayList<Performance.Artist> getSavelist3() {
+        return savelist3;
+    }
+
+    public void setSavelist3(ArrayList<Performance.Artist> savelist3) {
+        this.savelist3 = savelist3;
+    }
+
+    public ArrayList<Performance.Artist> getSavelist4() {
+        return savelist4;
+    }
+
+    public void setSavelist4(ArrayList<Performance.Artist> savelist4) {
+        this.savelist4 = savelist4;
+    }
+
+
 
     public void saveFile(Artist artist){
         if(GUI.saveFile == file1) {
